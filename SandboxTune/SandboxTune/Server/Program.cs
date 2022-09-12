8200d439f38d5408e39e5b61ab756b6e70e635c5
amal <nuramalinamohdrosli.work@gmail.com>
2022-09-12 11:36:21 +0800

    add docker file

--- SandboxTune/SandboxTune/Server/Program.cs
+++ SandboxTune/SandboxTune/Server/Program.cs
@@ -6,9 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -28,7 +28,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blazor API V1");
+    c.RoutePrefix = "docs";
+});
 app.MapRazorPages();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
